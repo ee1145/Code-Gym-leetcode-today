644c76607bea62d9de6f0150aae2fa203a998b43
Zhuomin Li <41363479+ee1145@users.noreply.github.com>
2026-05-10 18:11:02 -0500

    05-10-2026 0347-top-k-frequent-elements .java

---
+++ Java/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements-05-10-2026-18-11-01.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // 第一步：统计频率
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // 第二步：小顶堆，按频率排序
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
+        );
+
+        for (int num : map.keySet()) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        // 第三步：取出结果
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pq.poll();
+        }
+        return result;
+    }
+}
