fa6eac67c83eb33eab91a9e18362bbf9be7d0d95
Zhuomin Li <41363479+ee1145@users.noreply.github.com>
2026-05-24 15:42:22 -0500

    05-24-2026 0973-k-closest-points-to-origin .java

---
+++ Java/Medium/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin-05-24-2026-15-42-22.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    private int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(dist(b), dist(a))  // Max-Heap
+        );
+
+        for (int[] point : points) {
+            heap.offer(point);
+            if (heap.size() > k) heap.poll();
+        }
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = heap.poll();
+        }
+        return result;
+    }
+}
