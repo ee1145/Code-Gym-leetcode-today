b018c74aeb3e624470ddac44d1a7943d9e5b842f
Zhuomin Li <41363479+ee1145@users.noreply.github.com>
2026-06-22 21:43:43 -0500

    06-22-2026 0155-min-stack .java

---
+++ Java/Medium/0155-min-stack/0155-min-stack-06-22-2026-21-43-43.java
@@ -0,0 +1,37 @@
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+        int min = minStack.isEmpty() ? value : Math.min(value, minStack.peek());
+        minStack.push(min);
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
