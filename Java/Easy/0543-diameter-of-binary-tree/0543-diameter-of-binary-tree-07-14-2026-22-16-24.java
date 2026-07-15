cfadc9b3fc3c829763b534906e5c2a0d99224187
Zhuomin Li <41363479+ee1145@users.noreply.github.com>
2026-07-14 22:16:25 -0500

    07-14-2026 0543-diameter-of-binary-tree .java

---
+++ Java/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree-07-14-2026-22-16-24.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiameter = 0;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return maxDiameter;
+    }
+    
+    private int depth(TreeNode node) {
+        if (node == null) return 0;
+        
+        int left = depth(node.left);
+        int right = depth(node.right);
+        
+        // 更新最大直径
+        maxDiameter = Math.max(maxDiameter, left + right);
+        
+        // 返回当前节点的深度
+        return Math.max(left, right) + 1;
+    }
+}
